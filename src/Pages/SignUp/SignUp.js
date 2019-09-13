098dc6db2a2904f9c99c910a1779ca91f22fc7b1
yiqiong <yiqiong97@gmail.com>
2019-09-13 20:24:33 +0800

    state for validation: pending backend for API

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,126 +1,75 @@
 import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import Link from '@material-ui/core/Link';
-import { Link as RouterLink, Route, BrowserRouter, Switch } from 'react-router-dom';
-import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import Login from '../Login/Login.js'
+import SignUpView from './SignUpView.js'
 
-const useStyles = makeStyles(theme => ({
-  '@global': {
-    body: {
-      backgroundColor: theme.palette.common.white,
-    },
-  },
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(3),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
 
-export default function SignUp() {
-  const classes = useStyles();
 
-  return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign up
-        </Typography>
-        <form className={classes.form} noValidate>
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                
-                name="firstName"
-                variant="outlined"
-                required
-                fullWidth
-                id="firstName"
-                label="First Name"
-                autoFocus
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="lastName"
-                label="Last Name"
-                name="lastName"
-                
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="username"
-                label="Username"
-                name="username"
-                
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                
-              />
-            </Grid>
-            
-          </Grid>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign Up
-          </Button>
-          <Grid container justify="flex-end">
-            <Grid item>
-             <Link variant="body2">
-            <RouterLink to="./signin" style={{textDecoration : 'inherit', color: 'inherit'}}>
-                {" Already have an account? Sign in"}
-              </RouterLink>
-               </Link>
+class SignUp extends React.Component {
+  constructor (props) {
+    super(props);
+    // Bind the this context to the handler function
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
 
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-    </Container>
-  );
+    this.state = {
+      email: '',
+      password:'',
+      emailValid: false,
+      passwordValid: false,
+      formValid: false,
+      submitForm:false
+    }
+
+  }
+
+  // This method will be sent to the child component
+  //for validation onChange for input fields
+  handleChange (e) {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState({[name]: value}, 
+      () => { this.validateField(name, value) });
+  }
+
+  validateField(fieldName, value) {
+    let fieldValidationErrors = this.state.formErrors;
+    let emailValid = this.state.emailValid;
+    let passwordValid = this.state.passwordValid;
+  
+    switch(fieldName) {
+      case 'email':
+        emailValid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value);       
+        break;
+      case 'password':
+        passwordValid = value.length >= 6;
+        break;
+      default:
+        break;
+    }
+    this.setState({emailValid: emailValid,
+                    passwordValid: passwordValid
+                  }, this.validateForm);
+  }
+  
+  validateForm() {
+    this.setState({formValid: this.state.emailValid && this.state.passwordValid});
+    //GOT PROBLEM formValid is false even thou emailValid and passwordValid are true
+    console.log(this.state.formValid,this.state.emailValid,this.state.passwordValid);
+  }
+
+  handleSubmit(event) { 
+    this.setState({submitForm:true}) // only render errors when form is submitted
+    console.log("Form submitted");
+    event.preventDefault();
+    if (this.state.formValid){
+      // API FROM ERN TEK
+    } 
+  }
+  
+
+  render () {
+    return(<SignUpView handleChange = {this.handleChange} handleSubmit = {this.handleSubmit} state= {this.state}/>);
+    
+  }
 }
+
+export default SignUp;
