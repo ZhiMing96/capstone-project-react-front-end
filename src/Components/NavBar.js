a088916848a97a3134a28aceedee0d18d5a138d3
yiqiong <yiqiong97@gmail.com>
2019-09-19 17:36:55 +0800

    sign up redirect to login

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -20,19 +20,6 @@ import { connect } from "react-redux";
 import PersonIcon from '@material-ui/icons/Person';
 import Fab from '@material-ui/core/Fab';
 
-function TabContainer({ children, dir }) {
-  return (
-    <Typography component="div" dir={dir} style={{ padding: 8 * 3 }}>
-      {children}
-    </Typography>
-  );
-}
-
-TabContainer.propTypes = {
-  children: PropTypes.node.isRequired,
-  dir: PropTypes.string.isRequired
-};
-
 
 class NavTabs extends React.Component {
 
