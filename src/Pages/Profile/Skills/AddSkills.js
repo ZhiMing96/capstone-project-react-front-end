578a60ae6a5f451d7798fc5dfaf1bcba3f55192f
yiqiong <yiqiong97@gmail.com>
2019-09-27 22:43:39 +0800

    changd the remove skills flow and ui

--- src/Pages/Profile/Skills/AddSkills.js
+++ src/Pages/Profile/Skills/AddSkills.js
@@ -15,188 +15,202 @@ import Chip from '@material-ui/core/Chip';
 import { Box } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
-    root: {
-      flexGrow: 1,
-    },
-    container: {
-      position: 'relative',
-    },
-    suggestionsContainerOpen: {
-      position: 'absolute',
-      zIndex: 1,
-      marginTop: theme.spacing(1),
-      left: 0,
-      right: 0,
-    },
-    suggestion: {
-      display: 'block',
-    },
-    suggestionsList: {
-      margin: 0,
-      padding: 0,
-      listStyleType: 'none',
-    },
-    divider: {
-      height: theme.spacing(1),
-    },
+  root: {
+    flexGrow: 1,
+  },
+  container: {
+    position: 'relative',
+  },
+  suggestionsContainerOpen: {
+    position: 'absolute',
+    zIndex: 1,
+    marginTop: theme.spacing(1),
+    left: 0,
+    right: 0,
+  },
+  suggestion: {
+    display: 'block',
+  },
+  suggestionsList: {
+    margin: 0,
+    padding: 0,
+    listStyleType: 'none',
+  },
+  divider: {
+    height: theme.spacing(1),
+  },
 }));
 
-const suggestions =  
-[
-    {
-        "id": "1232",
-        "skill": "Cellular"
-    },
-    {
-        "id": "8493",
-        "skill": "Sporting Goods"
-    },
-    {
-        "id": "4433",
-        "skill": "Information Security Policy"
-    },
-    {
-        "id": "34",
-        "skill": "ABR"
-    },
-    {
-        "id": "1",
-        "skill": "hello world"
-    },
-];
+const suggestions = [];
 
 function renderInputComponent(inputProps) {
-    const { classes, inputRef = () => {}, ref, ...other } = inputProps;
-  
-    return (
-      <TextField
-        variant="outlined"
-        fullWidth
-        InputProps={{
-          inputRef: node => {
-            ref(node);
-            inputRef(node);
-          },
-          classes: {
-            input: classes.input,
-          },
-          endAdornment: (
-            <InputAdornment position="end">
-                <IconButton
-                edge="end">
-                    <AddBoxIcon/>
-                </IconButton>
-            </InputAdornment>),
-        }}
-        {...other}
+  const { classes, inputRef = () => { }, ref, ...other } = inputProps;
 
-      />
-    );
+  return (
+    <TextField
+      variant="outlined"
+      fullWidth
+      InputProps={{
+        inputRef: node => {
+          ref(node);
+          inputRef(node);
+        },
+        classes: {
+          input: classes.input,
+        },
+        endAdornment: (
+          <InputAdornment position="end">
+            <IconButton
+              edge="end">
+              <AddBoxIcon />
+            </IconButton>
+          </InputAdornment>),
+      }}
+      {...other}
+
+    />
+  );
 }
 
-async function getSuggestions( value) {
-    console.log('getting suggestions')
-    console.log(value)
-    return suggestions //(this works)
+async function getSuggestions(value) {
+  console.log('getting suggestions')
+  console.log(value)
+  return [{
+    "id": "1232",
+    "skill": "Cellular"
+  },
+  {
+    "id": "8493",
+    "skill": "Sporting Goods"
+  },
+  {
+    "id": "4433",
+    "skill": "Information Security Policy"
+  },
+  {
+    "id": "34",
+    "skill": "ABR"
+  },
+  {
+    "id": "1",
+    "skill": "hello world"
+  },] //(this works, dummy)
 
-    await api.skills.match({"skill": value})
-    .then(response=>{
-        if (response.data.response_code===200){
-            return response.data.skills
-        }else{
-            var error = response.data.response_message
-            return []
-        }
-    }).catch(error=>{
-        var error = "Error retrieving data"
+  await api.skills.match({ "skill": value })
+    .then(response => {
+      if (response.data.response_code === 200) {
+        return response.data.skills
+      } else {
+        var error = response.data.response_message
         return []
+      }
+    }).catch(error => {
+      var error = "Error retrieving data"
+      return []
     })
 };
 
 export default function IntegrationAutosuggest(props) {
-    const classes = useStyles();
-    const [state, setState] = React.useState('');
-    const [stateSuggestions, setSuggestions] = React.useState([]);
-    const[currentSkills, setSkills] = React.useState([]);
+  const classes = useStyles();
+  const [state, setState] = React.useState('');
+  const [stateSuggestions, setSuggestions] = React.useState([]);
+  const [currentSkills, setSkills] = React.useState([{
+    "id": "1232",
+    "skill": "Cellular"
+  },
+  {
+    "id": "8493",
+    "skill": "Sporting Goods"
+  },
+  {
+    "id": "4433",
+    "skill": "Information Security Policy"
+  },
+  {
+    "id": "34",
+    "skill": "ABR"
+  },]);//dummy (supposed to be [])
 
-  
-    const handleSuggestionsFetchRequested = async ({ value }) => {
-      setSuggestions(await getSuggestions(value));
-    };
-  
-    const handleSuggestionsClearRequested = () => {
-      setSuggestions([]);
-    };
-  
-    const handleChange = ()=>(event, { newValue,method }) => {
-        setState(newValue)
+  const handleSuggestionsFetchRequested = async ({ value }) => {
+    if(value === null || value ===''){
+      return
     }
+    setSuggestions(await getSuggestions(value));
+  };
 
-    const handleSuggestionSelected = (event, { suggestion, suggestionValue, suggestionIndex, sectionIndex, method }) => {
-        console.log(suggestion.skill + " onSuggestionSelected");
-        props.handleSelectedSkill(suggestion)
-        setSkills(props.skills)
-    };
+  const handleSuggestionsClearRequested = () => {
+    setSuggestions([]);
+  };
 
-    const renderSuggestion=(suggestion, { query, isHighlighted }) =>{
-        const matches = match(suggestion.skill, query);
-        const parts = parse(suggestion.skill, matches);
-      
-        return (
-          <MenuItem selected={isHighlighted} >
-            <span>
-              {parts.map(part => (
-                <span key={part.text} style={{ fontWeight: part.highlight ? 500 : 400 }}>
-                  {part.text}
-                </span>
-              ))}
-              
-            </span>
-            {currentSkills.some(skill => skill.id === suggestion.id) &&
-            <span style={{marginLeft: '10px'}}>
-                <Chip label="Added"  color='primary' size='small'/>
-            </span>
-            }
-          </MenuItem>
-        )
-    };
+  const handleChange = () => (event, { newValue, method }) => {
+    setState(newValue)
+  }
 
-    const autosuggestProps = {
-      renderInputComponent,
-      suggestions: stateSuggestions,
-      onSuggestionsFetchRequested: handleSuggestionsFetchRequested,
-      onSuggestionsClearRequested: handleSuggestionsClearRequested,
-      getSuggestionValue:()=> {return state},
-      renderSuggestion,
-      alwaysRenderSuggestions:true,
-      onBlur: handleSuggestionsClearRequested,
-      onSuggestionSelected: handleSuggestionSelected,
-    };
-  
+  const handleSuggestionSelected = (event, { suggestion, suggestionValue, suggestionIndex, sectionIndex, method }) => {
+    console.log(suggestion.skill + " onSuggestionSelected");
+    props.handleSelectedSkill(suggestion)
+    setSkills(props.skills) //update skills state
+  };
+
+  const renderSuggestion = (suggestion, { query, isHighlighted }) => {
+    console.log('rendering suggestion')
+    const matches = match(suggestion.skill, query);
+    const parts = parse(suggestion.skill, matches);
+
     return (
-      <div className={classes.root}>
-        <Autosuggest
-          {...autosuggestProps}
-          inputProps={{
-            classes,
-            id: 'react-autosuggest-simple',
-            placeholder: 'Add your new skill',
-            value:state,
-            onChange: handleChange(),
-          }}
-          theme={{
-            container: classes.container,
-            suggestionsContainerOpen: classes.suggestionsContainerOpen,
-            suggestionsList: classes.suggestionsList,
-            suggestion: classes.suggestion,
-          }}
-          renderSuggestionsContainer={options => (
-            <Paper {...options.containerProps} square>
-              {options.children}
-            </Paper>
-          )}
-        />
-    </div>
+      <MenuItem selected={isHighlighted} >
+        <span>
+          {parts.map(part => (
+            <span key={part.text} style={{ fontWeight: part.highlight ? 500 : 400 }}>
+              {part.text}
+            </span>
+          ))}
+
+        </span>
+        {currentSkills.some(skill => skill.id === suggestion.id) &&
+          <span style={{ marginLeft: '10px' }}>
+            <Chip label="Added" color='primary' size='small' />
+          </span>
+        }
+      </MenuItem>
     )
+  };
+
+  const autosuggestProps = {
+    renderInputComponent,
+    suggestions: stateSuggestions,
+    onSuggestionsFetchRequested: handleSuggestionsFetchRequested,
+    onSuggestionsClearRequested: handleSuggestionsClearRequested,
+    getSuggestionValue: () => { return state },
+    renderSuggestion,
+    alwaysRenderSuggestions: true,
+    onBlur: handleSuggestionsClearRequested,
+    onSuggestionSelected: handleSuggestionSelected,
+  };
+
+  return (
+    <div className={classes.root}>
+      <Autosuggest
+        {...autosuggestProps}
+        inputProps={{
+          classes,
+          id: 'react-autosuggest-simple',
+          placeholder: 'Add your new skill',
+          value: state,
+          onChange: handleChange(),
+        }}
+        theme={{
+          container: classes.container,
+          suggestionsContainerOpen: classes.suggestionsContainerOpen,
+          suggestionsList: classes.suggestionsList,
+          suggestion: classes.suggestion,
+        }}
+        renderSuggestionsContainer={options => (
+          <Paper {...options.containerProps} square>
+            {options.children}
+          </Paper>
+        )}
+      />
+    </div>
+  )
 }
-  
--- src/Pages/Profile/Skills/CurrentSkillsView.js
+++ src/Pages/Profile/Skills/CurrentSkillsView.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import { Grid, Typography, Box } from '@material-ui/core'
-import IconButton from '@material-ui/core/IconButton';
-import DeleteIcon from '@material-ui/icons/Delete';
-import { borders } from '@material-ui/system';
 import api from '../../../api.js'
 import SnackBar from '../../../Components/Snackbar'
-import Chip from '@material-ui/core/Chip';
 import { makeStyles } from '@material-ui/core';
 import AddSkills from './AddSkills'
+import CustomisedChip from '../../../Components/CustomisedChip'
 
 
-const styles = makeStyles(theme=>({
+const styles = makeStyles(theme => ({
   root: {
     display: 'flex',
     justifyContent: 'center',
@@ -22,185 +19,151 @@ const styles = makeStyles(theme=>({
 
 
 class CurrentSkillsView extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
     this.handleRemove = this.handleRemove.bind(this);
     this.handleClose = this.handleClose.bind(this);
-    this.deleteState = this.deleteState.bind(this);
     this.handleSelectedSkill = this.handleSelectedSkill.bind(this);
     this.state = {
-      message:'' ,
-      variant:'',
+      message: '',
+      variant: '',
       skills: [],
-      delete:false,
-      openDialog:false,
-      newSkills:[]
+      newSkills: []
     }
   }
 
   async getSkills() {
     await api.skills.get(window.localStorage.getItem('authToken'))
-    .then(response => {
-      this.setState({skills:[
-        {
-            "id": "1232",
-            "skill": "Cellular"
-        },
-        {
-            "id": "8493",
-            "skill": "Sporting Goods"
-        },
-        {
-            "id": "4433",
-            "skill": "Information Security Policy"
-        },
-        {
-            "id": "34",
-            "skill": "ABR"
-        },
-        {
-        
-        "id": "1232",
-        "skill": "Cellular"
-        },
-        {
-            "id": "8493",
-            "skill": "Sporting Goods"
-        },
-        {
-            "id": "4433",
-            "skill": "Information Security Policy"
-        },
-        {
-            "id": "34",
-            "skill": "ABR"
-        }
-      ]}) 
-    }).catch(error => {
-      
-      console.log('issue w token')          
-    })
+      .then(response => {
+        this.setState({
+          skills: [
+            {
+              "id": "1232",
+              "skill": "Cellular"
+            },
+            {
+              "id": "8493",
+              "skill": "Sporting Goods"
+            },
+            {
+              "id": "4433",
+              "skill": "Information Security Policy"
+            },
+            {
+              "id": "34",
+              "skill": "ABR"
+            },
+          ]
+        })
+      }).catch(error => {
+
+        console.log('issue w token')
+      })
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.getSkills();
   }
 
-  async handleRemove (skillName,event) {
+  async handleRemove(skillName, event) {
     event.preventDefault()
-    console.log("Skill to remove: "+ skillName)
-    await api.skills.remove({"skill_remove": skillName})
-    .then((response) => {
-      console.log(response.data.response_code)
-  
-      if(response.data.response_code === 200){
-        this.setState({variant:'success'})
-        this.setState({message: skillName + ' removed from your skills.'})
-        this.setState(prevState=>({ skills: prevState.skills.filter(function(value, index, arr){
-          return value.skill !== skillName;
-      })}))
-        
-      } else if (response.data.response_code === 400){
-        this.setState({variant:'error'})
-        this.setState({message: response.data.response_message})
-        
-      } else {
-        this.setState({variant:'error'})
-        this.setState({message: 'Error in removing skill.'})
-        
-      }
-  
-    })
-    .catch(error => {
-      this.setState({variant:'error'})
-      this.setState({message: 'Error in removing skill.'})
-      
-    })
-  
+    console.log("Skill to remove: " + skillName)
+    await api.skills.remove({ "skill_remove": skillName })
+      .then((response) => {
+        console.log(response.data.response_code)
+
+        if (response.data.response_code === 200) {
+          this.setState({ variant: 'success' })
+          this.setState({ message: skillName + ' removed from your skills.' })
+          this.setState(prevState => ({
+            skills: prevState.skills.filter(function (value, index, arr) {
+              return value.skill !== skillName;
+            })
+          }))
+
+        } else if (response.data.response_code === 400) {
+          this.setState({ variant: 'error' })
+          this.setState({ message: response.data.response_message })
+
+        } else {
+          this.setState({ variant: 'error' })
+          this.setState({ message: 'Error in removing skill.' })
+
+        }
+
+      })
+      .catch(error => {
+        this.setState({ variant: 'error' })
+        this.setState({ message: 'Error in removing skill.' })
+
+      })
+
   };
-  
-  handleClose (reason){
+
+  handleClose(reason) {
     if (reason === 'clickaway') {
       return;
     }
-    this.setState({message: ''})
-  
+    this.setState({ message: '' })
+
   };
 
-  handleSelectedSkill(newSkill){ 
+  handleSelectedSkill(newSkill) {
     var id = newSkill.id
     var currentSkills = this.state.skills
-    if(currentSkills.some(skill => skill.id === id)){
+    if (currentSkills.some(skill => skill.id === id)) {
       console.log(newSkill.skill + " is already in current skills");
-    }else{
+    } else {
       console.log(newSkill.skill + " is now added to skills");
-      this.setState(prevState=>({ skills: prevState.skills.push(newSkill)}))
-      this.setState(prevState=>({ newSkills: prevState.newSkills.push(newSkill)}))
+      this.setState(prevState => ({ skills: prevState.skills.push(newSkill) }))
+      this.setState(prevState => ({ newSkills: prevState.newSkills.push(newSkill) }))
     }
 
   }
 
-  deleteState(){
-    this.setState(prevState=>({ delete: !prevState.delete}))
-    console.log(this.state.delete)
-  }
- 
-  render () {
-  const { classes } = this.props;
-  return(
-    <div>
-      <Grid container direction="row" style={{width:'100%'}}> {/*for your skills and search box and delete icon*/}
-        <Grid item xs={12} md ={12}>
-          <Typography component="div">
-              <Box 
-                fontSize="h6.fontSize" 
+  render() {
+    const { classes } = this.props;
+    return (
+      <div>
+        <Grid container direction="row" style={{ width: '100%' }}> {/*for your skills and search box and delete icon*/}
+          <Grid item xs={12} md={12}>
+            <Typography component="div">
+              <Box
+                fontSize="h6.fontSize"
                 m={2}
                 letterSpacing={2}
                 textAlign='left'
               >
-              
-              YOUR SKILLS 
+
+                YOUR SKILLS
               </Box>
-              </Typography>
+            </Typography>
           </Grid>
 
-          <Grid item style={{ width:'100%', paddingLeft:'2.5%', paddingRight:'2.5%'}}>
-            <Grid container style={{justifyContent:'space-between'}}> {/*for search box and delete icon*/}
-              <Grid item xs={9} md={9} style={{textAlign:'left'}}>
-                <AddSkills handleSelectedSkill={this.handleSelectedSkill} skills={this.state.skills}/>
-              </Grid>
-              <Grid item>
-                <IconButton className={classes.button} aria-label="delete" onClick={this.deleteState} style={{textAlign:'right'}}>
-                  <DeleteIcon fontSize="large" />
-                </IconButton>
-              </Grid>
-            </Grid>
+          <Grid item style={{ width: '100%', paddingLeft: '2.5%', paddingRight: '2.5%' }}>
+            <AddSkills handleSelectedSkill={this.handleSelectedSkill} skills={this.state.skills} />
           </Grid>
 
         </Grid>
 
-        <Grid container className={classes.root} style={{ padding:'2.5%'}}>
-          {this.state.delete ? 
-            this.state.skills.map(skill =>(
-              <Chip variant="outlined" color="primary" label={skill.skill}  clickable='false' style={{ margin:4}} onDelete={event=>this.handleRemove(skill.skill,event)}/>))
-            :
-            this.state.skills.map(skill =>(
-              <Chip variant="outlined" color="primary" label={skill.skill} clickable='false' style={{ margin:4}} />
-          ))}  
+        <Grid container className={classes.root} style={{ padding: '2.5%' }}>
+          {
+            this.state.skills.map(skill => (<CustomisedChip skill={skill} handleRemove={this.handleRemove}/>))
+          }
         </Grid>
 
-      <SnackBar
-        open={this.state.message!==''}
-        handleClose={this.handleClose}
-        variant={this.state.variant}
-        message={this.state.message}
-      />
-      
+        <SnackBar
+          open={this.state.message !== ''}
+          handleClose={this.handleClose}
+          variant={this.state.variant}
+          message={this.state.message}
+        />
 
-    </div>
-    
-  );
-}
+
+      </div>
+
+    );
+  }
 }
 
 export default withStyles(styles)(CurrentSkillsView);
