4678f71573e1ee86cc3cf20176021c5ed953573b
ZhiMing96 <e0200805@u.nus.edu>
2019-10-15 04:57:58 +0800

    Created New Events Page with Font End Rendering Functionalities (Dummy Gmaps Img)

--- src/Pages/Events/index.js
+++ src/Pages/Events/index.js
@@ -1,6 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import Axios from 'axios';
-import { Grid, Paper, Typography, ButtonBase, makeStyles } from '@material-ui/core';
+import React, { useState, useEffect, Fragment } from 'react';
+import axios from 'axios';
+import { Grid, Paper, Typography, ButtonBase, makeStyles, CssBaseline, Box, Divider, Tabs, Tab, Hidden, Button } from '@material-ui/core';
+import { wrap } from 'module';
+import { light } from '@material-ui/core/styles/createPalette';
+import { maxHeight } from '@material-ui/system';
+import {Info as InfoIcon}  from '@material-ui/icons';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -14,29 +18,64 @@ const useStyles = makeStyles(theme => ({
     maxWidth: 700,
   },
   image: {
-    width: 128,
-    height: 128,
+    maxWidth: '90%',
+    maxHeight: '90%',
+    // paddingTop: 15
+    // padding: '15px 10px '
   },
   img: {
-    margin: 'auto',
-    display: 'block',
-    maxWidth: '100%',
-    maxHeight: '100%',
+    // [theme.breakpoints.up('md')]:{
+    //   padding: '10px 10px',
+    //   width:'fit',
+    //   height:'fit',
+    //   maxWidth: 140 ,
+    //   maxHeight: 170 ,
+    // },
+    // [theme.breakpoints.down('sm')]:{
+    //   padding: '10px 10px',
+    //   width:'fit',
+    //   height:'fit',
+    //   maxWidth: 100 ,
+    //   maxHeight: 130 ,
+    // }
+    width: '70%',
+    height: '80%',
+    maxWidth:'90%',
+    maxHeight:'115px'
   },
 }));
 
 function Events() {
   const classes = useStyles();
   const [events, setEvents] = useState([]);
-
+  const [recommendedEvents, setRecommendedEvents] = useState([]);
+  const [dailyPicks, setDailyPicks] = useState([]);   
+  const [value, setValue] = React.useState(0);
+  const [fontWeight, setFontWeight] = useState({
+    recommended: '600',
+    topPicks: '300',
+    latest:'300'
+  })
+  const [selectedEventLocation, setSelectedEventLocation] = useState("Please Select An Event");
+  const [selectedEventDescription, setSelectedEventDescription] = useState("Please Select An Event");
+  
   useEffect(() => {
-    Axios.get('https://portal.ssg-wsg.gov.sg/content/web/eventsfeed/eventlisting.xml')
+    var filter= '';
+    if(value===0){
+      filter='recommended'
+    } else if (value===1){
+      filter='top-picks'
+    } else {
+      filter='latest'
+    }
+    console.log(filter);
+
+    axios.get('https://portal.ssg-wsg.gov.sg/content/web/eventsfeed/eventlisting.xml')
     .then(res => {
-      //console.log(res.data)
+      console.log(res.data)
       const data = res.data
       const parser = new DOMParser()
       const xmlDoc = parser.parseFromString(data, 'text/xml')
-
       const length = xmlDoc.getElementsByTagName("event").length;
       // console.log(length);
 
@@ -50,9 +89,11 @@ function Events() {
         const eventSegment = xmlDoc.getElementsByTagName("eventSegment")[i].innerHTML
         const eventPrice = xmlDoc.getElementsByTagName("price")[i].innerHTML
         const eventVenue = xmlDoc.getElementsByTagName("venueName")[i].innerHTML
-        const location = xmlDoc.getElementsByTagName("locationType")[i].innerHTML
+        const buildingName = xmlDoc.getElementsByTagName("buildingName")[i].innerHTML
         const postalCode = xmlDoc.getElementsByTagName("postalcode")[i].innerHTML
+        const streetName = xmlDoc.getElementsByTagName("streetName")[i].innerHTML
         const targetAudience = xmlDoc.getElementsByTagName("targetAudience")[i].innerHTML
+        const targetNationality = xmlDoc.getElementsByTagName("targetNationality")[i].innerHTML
 
         const eventGathered = {
           eventName: eventName,
@@ -64,77 +105,263 @@ function Events() {
           eventSegment: eventSegment,
           eventPrice: eventPrice,
           eventVenue: eventVenue,
-          location: location,
+          buildingName: buildingName,
           postalCode: postalCode,
+          streetName: streetName,
           targetAudience: targetAudience,
+          targetNationality: targetNationality,
+          
         }
 
         setEvents(events => [...events, eventGathered]);
-
         
-        
-        // console.log(eventName + "\n" + eventUrl + "\n" + eventDescription + "\n" + eventImgUrl + "\n" + eventStartDate + "\n" + eventEndDate + "/\n" + eventSegment + "\n" + eventPrice + "\n" + eventVenue + "\n" + location + "\n" + postalCode);
+        console.log(eventName + "\n" + eventUrl + "\n" + eventImgUrl + "\n" + eventStartDate + "\n" + eventEndDate + "/\n" + eventSegment + "\n" + eventPrice + "\n" + eventVenue + "\n" + buildingName + "\n" + postalCode + "\n" + streetName);
       }
       console.log(xmlDoc);
     })
-  },[]);
+  },[value]);
+
+  const handleChange = (event, newValue) => {
+    console.log(newValue) 
+    switch(newValue) {
+      case 0:
+        setFontWeight({
+          recommended: '600',
+          topPicks: '300',
+          latest:'300'
+        })
+        break;
+      case 1:
+        setFontWeight({
+          recommended: '300',
+          topPicks: '600',
+          latest:'300'
+        })
+      break;
+      case 2:
+          setFontWeight({
+            recommended: '300',
+            topPicks: '300',
+            latest:'600'
+          })
+      break;
+      
+    }setValue(newValue);
+  };
+
+  const getDate =(startDate, endDate) => {
+    var newStartDate = new Date(startDate);
+    var newEndDate = new Date(endDate);
+    //console.log(newStartDate.getHours());
+    var startTime = newStartDate.getHours()
+    var endTime = newEndDate.getHours()
+
+    if(startTime <= 12){
+      startTime = `${startTime}am`
+      //console.log(startTime);
+    } else {
+      startTime = `${startTime-12}pm`
+      //console.log(startTime);
+    }
+    if(endTime <= 12){
+      endTime = `${endTime}am`
+      //console.log(endTime);
+    } else {
+      endTime = `${endTime-12}pm`
+      //console.log(endTime);
+    }
+
+    var month = newStartDate.toLocaleString('en-GB', {month: 'short'});
+    console.log(month);
+    var startDay = newStartDate.getMonth();
+    var endDay = newEndDate.getMonth();
+    var startYear = newStartDate.getFullYear();
+
+    var day = startDay
+    if(startDay != endDay){
+      day = `${startDay}-${endDay}`
+    }
+    
+    return (`${day} ${month} ${startYear}: ${startTime}-${endTime}`)
 
 
+
+    //  5-6 Oct 2019: 1pm-5pm
+    // var parsedStartDate = newStartDate.toLocaleString('en-GB', {
+    //   day: 'numeric',
+    //   month: 'short',
+    //   year: 'numeric',
+    //   hour: '2-digit',
+    //   minute: '2-digit',
+    // })
+    //var startTime = `${newStartDate.getTime}`
+    //console.log(parsedDate)
+    // console.log(newDate.getMonth());
+    // console.log(newDate.getDate());
+    // console.log(newDate.getFullYear());
+    //
+  }
+
+  const formatVenue = (building, venue, streetName, postalCode) =>{
+    //Lifelong Learning Center: Training Room 2-1, Eunos Road 9 S123456
+
+    if(postalCode!== '000000' && streetName!=='-'){
+      return (`${building}: ${venue}, ${streetName} S${postalCode}`);
+    } else {
+      return ('Location Unavailable')
+    }
+  }
+
+  const viewDetails = (event) =>{
+    console.log(event)
+    setSelectedEventDescription(event.eventDescription);
+    const location = formatVenue(event.buildingName,event.eventVenue,event.streetName,event.postalCode)
+    setSelectedEventLocation(location);
+    console.log('Exiting view Event Details')
+  }
 
   return(
     <div>
-      <h2> Events </h2>
+      <Fragment>
+        <CssBaseline/>
+        
+          <Box display="flex" flexDirection="row">
+          <div style={{ width: '100%' }}>
+            <Typography>
+              <Box component="span" display="block" textAlign="left" fontWeight={600} fontSize={40} letterSpacing={2} style={{marginLeft:'10%', marginTop:30}} width="100%">
+                EVENTS
+              </Box>
+            </Typography>
+            <Grid container style={{marginLeft:'10%'}}>
+              <Divider orientation="vertical" style={{width:7, height:30, backgroundColor :'#1382B9', marginRight:10}} />
+              <Typography style={{marginTop:5, fontWeight:600}}>
+                One Stop Career Guidance Portal
+              </Typography>
+            </Grid>
+            </div>
+          </Box>
 
-      <div className={classes.root}>
+          
+          <div style={{maxWidth: '100%', marginTop:10, flex:'wrap',  overflow:"auto"}}>
+         
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              indicatorColor=""
+              textColor=""
+              centered
+              style={{maxWidth:'100%', margin:30}}
+            >
+                <Tab disableRipple style={{minHeight:0, padding:'3px 12px',}}  label={<span style={{fontWeight:fontWeight.recommended}}>Recommended</span>} />
+                <Tab disableRipple style={{minHeight:0, padding:'3px 12px'}} label={<span style={{fontWeight:fontWeight.topPicks}}>Top Picks</span>} />
+                <Tab disableRipple style={{minHeight:0, padding:'3px 12px'}} label={<span style={{fontWeight:fontWeight.latest}}>Latest</span>} />
+            </Tabs>
+          </div>
+          
+        <Grid container>
+          <Grid item lg={8} sm={7} style={{maxHeight: 700,overflowY:'auto'}}>
           {events.map((event, index) => (
               <div key={index}>
-                  <a href={event.eventUrl} style={{textDecoration: 'none', color: 'black'}}>
-                      <Paper className={classes.paper}>
-                      <Grid container spacing={2}>
-                          <Grid item>
-                          <ButtonBase className={classes.image}>
-                              <img className={classes.img} alt="complex" src={event.eventImgUrl} />
-                          </ButtonBase>
-                          </Grid>
-                          <Grid item xs={12} sm container>
-                          <Grid item xs container direction="column" spacing={2}>
-                              <Grid item xs>
-                              <Typography gutterBottom variant="subtitle1">
-                                  {event.eventName}
-                              </Typography>
-                              <Typography variant="body2" gutterBottom>
-                                  Expiry Date: {event.eventEndDate}
-                              </Typography>
-                              <Typography variant="body2" color="textSecondary">
-                                  ID: 1030114
-                              </Typography>
-                              </Grid>
-                              <Grid item>
-                              <Typography variant="body2" style={{ cursor: 'pointer' }}>
-                                  Event is Suitable for: {
-                                    (event.targetAudience === "N/A") ? "No Match" : `${event.targetAudience}`
-                                  }
-                              </Typography>
-                              </Grid>
-                          </Grid>
-                          <Grid item>
-                              <Typography variant="subtitle1">
-                                {
-                                  (event.eventPrice === "$0.00") 
-                                  ? "FREE"
-                                  : `Cost: ${event.eventPrice}`
-                                }
-                              </Typography>
-                          </Grid>
-                          </Grid>
+                <Paper style={{marginInlineStart:20, height:130, marginInlineEnd:20, marginBottom:10, padding:10}}>
+                  <Grid container style={{height:'100%'}}>
+                    <Hidden xsDown>
+                      <Grid item lg={3} sm={4}>
+                            <ButtonBase className={classes.image} style={{flexFlow: 'wrap-reverse'}}>
+                                <img className={classes.img} alt="complex" src={event.eventImgUrl} />
+                            </ButtonBase>
                       </Grid>
-                      </Paper>
-                  </a>
-                  
+                    </Hidden>
+                    <Grid item lg={9} sm={8} xs={12} container style={{}}>
+                      <Grid item md={7} xs={12}>
+                        <Typography style={{paddingTop:10}}>
+                          <Box textAlign="left" fontWeight="fontWeightBold" fontSize={15}>
+                            {event.eventName}
+                          </Box>
+                          <Box textAlign="left" fontWeight={550} fontSize={13} style={{color:'#607d8b'}}>
+                            {getDate(event.eventStartDate,event.eventEndDate )}
+                          </Box>
+                          <Box textAlign="left" fontWeight={510} fontSize={12}>
+                            {event.buildingName !=='0' && event.buildingName !=='-'
+                            ? `${event.buildingName}, Singapore`
+                            : 'Singapore'
+                            }
+                          </Box>
+                          <Grid
+                            container
+                            direction="column"
+                            justify="flex-end"
+                            alignItems="flex-start"
+                          >
+                            <Grid item>
+                              <Box textAlign="left" fontWeight={510} fontSize={10}>
+                                {event.eventPrice !== '$0.00'
+                                ? event.eventPrice
+                                : 'FREE'
+                                }
+                              </Box>
+                            </Grid>
+                          </Grid>
+                        </Typography>
+                      </Grid>
+                      <Hidden smDown>
+                        <Grid item xs={5} style={{marginTop:10}}>
+                          <Button
+                          disableRipple={true}
+                          size='small'
+                          onClick={() => viewDetails(event)}
+                          color='primary'>
+                            View Details
+                          </Button>
+                          <Typography style={{fontSize:11, fontWeight:500}}>
+                            {event.targetNationality}
+                          </Typography>
+                          <Typography style={{fontSize:11, fontWeight:500}}>
+                            {event.eventSegment}
+                          </Typography>
+                        </Grid>
+                      </Hidden>
+                    </Grid>
+                  </Grid>
+                </Paper>
               </div>
-          // </div>
           ))}
-      </div>
+          </Grid>
+          <Grid item lg={4} sm={5}  style={{position:'sticky', paddingRight:20,maxHeight: 700}}>
+          <img src='https://i.stack.imgur.com/B6fEt.png' alt='img'style={{maxHeight:'100%', maxWidth:'100%'}}/>
+            <Paper style={{width:'100%', height:'50%'}}>
+              <Typography style={{fontWeight:'bold'}}>
+                DETAILS OF LOCATION 
+              </Typography>
+              <Typography style={{}}>
+                {selectedEventLocation}
+              </Typography>
+              <Typography style={{fontWeight:'bold'}}>
+                DESCRIPTION 
+              </Typography>
+              {/* <Typography style={{}}> */}
+              <div style={{width:'80%', margin:30, backgroundColor:'#EDF7FA', height:'fit-content', padding:5, maxHeight:'100%'}}>
+                          <Grid container style={{maxHeight:'100%'}}>
+                            <Grid item xs={1}>
+                              <Divider display='inline' orientation='vertical' style={{width:5, height:'100%', backgroundColor :'#1382B9'}} />
+                            </Grid>
+                            <Grid item xs={11}>
+                            <div style={{overflow: "hidden", textOverflow: "ellipsis"}}>
+                              <Typography style={{fontSize:10}} noWrap={true}>
+                                  <Box whiteSpace="normal" maxHeight='100px' textAlign="left">
+                                  {selectedEventDescription} 
+                                  </Box>
+                              </Typography>
+                            </div>
+                            </Grid>
+                          </Grid>
+                        </div>
+                
+              {/* </Typography> */}
+            </Paper>
+          </Grid>
+
+        </Grid>
+      </Fragment>
     </div>
   
   );
